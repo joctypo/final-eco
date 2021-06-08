4265638ca4a3aea11879948669bb17a56386208a
joctypo <oscargiraldop1999@gmail.com>
2021-06-08 10:19:29 -0500

    showing projects

--- app/src/main/java/com/joctypo/finaleco/HomeActivity.java
+++ app/src/main/java/com/joctypo/finaleco/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.joctypo.finaleco;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,54 +9,134 @@ import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Calendar;
 import java.util.UUID;
 
 public class HomeActivity extends AppCompatActivity {
 
-    ImageView btnProfile,btn2d,btn3d,btnvideo;
+    ImageView btnProfile, btn2d, btn3d, btnvideo;
     Button btnPublishProject;
     EditText etProjectDescription;
     String category;
     FirebaseDatabase db;
     FirebaseAuth auth;
+    ProjectAdapter projectAdapter;
+    FirebaseUser user;
+    ListView listViewProjects;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        btnProfile =  findViewById(R.id.btnProfile);
+        btnProfile = findViewById(R.id.btnProfile);
         btn2d = findViewById(R.id.img2d);
         btn3d = findViewById(R.id.img3d);
-        btnvideo =  findViewById(R.id.imgVideo);
-        btnPublishProject =  findViewById(R.id.btnPublishProject);
-        etProjectDescription=findViewById(R.id.etProjectDescription);
-        db=FirebaseDatabase.getInstance();
-        auth=FirebaseAuth.getInstance();
-        category="";
+        projectAdapter = new ProjectAdapter();
+        btnvideo = findViewById(R.id.imgVideo);
+        btnPublishProject = findViewById(R.id.btnPublishProject);
+        etProjectDescription = findViewById(R.id.etProjectDescription);
+        db = FirebaseDatabase.getInstance();
+        auth = FirebaseAuth.getInstance();
+        listViewProjects=findViewById(R.id.listViewProjects);
+        category = "";
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        LoadProjects();
 
-        btnPublishProject.setOnClickListener(v->{
 
-            if(etProjectDescription.getText().toString().isEmpty()){
+        //creacion del projecto
+        btnPublishProject.setOnClickListener(v -> {
 
-                Toast.makeText(this, "Necesitas agregar una descripción a tu proyecto",Toast.LENGTH_SHORT).show();
+            CreateProject();
 
-            }
-            else if(category.equals("")){
+        });
 
-                Toast.makeText(this,"por favor seleccione una categoria",Toast.LENGTH_SHORT).show();
-            }
-            else{
+        btn2d.setOnClickListener(v -> {
 
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            btn2d.setImageResource(R.drawable.ic_diseno_2d);
+            btn3d.setImageResource(R.drawable.ic_modelado3d_nobg);
+            btnvideo.setImageResource(R.drawable.ic_fotografia_nobg);
+            category = "diseño2D";
+        });
 
-                if(user!=null){
+        btn3d.setOnClickListener(v -> {
+
+            btn3d.setImageResource(R.drawable.ic_diseno_3d);
+            btn2d.setImageResource(R.drawable.ic_diseno2d_nobg);
+            btnvideo.setImageResource(R.drawable.ic_fotografia_nobg);
+            category = "modelado3D";
+        });
+
+        btnvideo.setOnClickListener(v -> {
+
+            btn3d.setImageResource(R.drawable.ic_modelado3d_nobg);
+            btn2d.setImageResource(R.drawable.ic_diseno2d_nobg);
+            btnvideo.setImageResource(R.drawable.ic_fotografia);
+            category = "video";
+        });
+
+
+        //boton del menu que lleva al perfil
+        btnProfile.setOnClickListener(v -> {
+
+            Intent intent = new Intent(this, ProfileActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    private void LoadProjects() {
+
+        if(user != null){
+            db.getReference().child("projects").orderByChild("userId").equalTo(user.getUid()).addValueEventListener( new ValueEventListener(){
+
+
+                @Override
+                public void onDataChange( DataSnapshot snapshot) {
+                    listViewProjects.setAdapter(projectAdapter);
+                    Log.e("TAG", String.valueOf(snapshot.getChildrenCount()));
+
+                    projectAdapter.Clear();
+
+                    for (DataSnapshot child :snapshot.getChildren()) {
+
+                        Project project = child.getValue(Project.class);
+                        projectAdapter.AddNewProject(project);
+                    }
+
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+
+                }
+            });
+        }
+
+    }
+
+    private void CreateProject() {
+        if (etProjectDescription.getText().toString().isEmpty()) {
+
+            Toast.makeText(this, "Necesitas agregar una descripción a tu proyecto", Toast.LENGTH_SHORT).show();
+
+        } else if (category.equals("")) {
+
+            Toast.makeText(this, "por favor seleccione una categoria", Toast.LENGTH_SHORT).show();
+        } else {
+
+
+
+            if (user != null) {
 
                 String id = UUID.randomUUID().toString();
 
@@ -63,53 +144,23 @@ public class HomeActivity extends AppCompatActivity {
                 int day = cal.get(Calendar.DAY_OF_MONTH);
                 int month = cal.get(Calendar.MONTH);
 
-                Project project = new Project(id, user.getUid(), etProjectDescription.getText().toString(),category,day,month);
 
-                db.getReference().child("projects").child(id).setValue(project).addOnCompleteListener(task->{
 
-                    if(task.isSuccessful()){
+                Project project = new Project(id, user.getUid(), etProjectDescription.getText().toString(), category, day, month);
 
-                        Toast.makeText(this, "proyecto subido correctamente",Toast.LENGTH_SHORT).show();
+                db.getReference().child("projects").child(id).setValue(project).addOnCompleteListener(task -> {
+
+                    if (task.isSuccessful()) {
+
+                        Toast.makeText(this, "proyecto subido correctamente", Toast.LENGTH_SHORT).show();
                         etProjectDescription.setText("");
                     }
                 });
 
-                }
-
             }
 
-        });
-
-        btn2d.setOnClickListener(v->{
-
-           btn2d.setImageResource(R.drawable.ic_diseno_2d);
-           btn3d.setImageResource(R.drawable.ic_modelado3d_nobg);
-           btnvideo.setImageResource(R.drawable.ic_fotografia_nobg);
-           category="diseño2D";
-        });
-
-        btn3d.setOnClickListener(v->{
-
-            btn3d.setImageResource(R.drawable.ic_diseno_3d);
-            btn2d.setImageResource(R.drawable.ic_diseno2d_nobg);
-            btnvideo.setImageResource(R.drawable.ic_fotografia_nobg);
-            category="modelado3D";
-        });
-
-        btnvideo.setOnClickListener(v->{
-
-            btn3d.setImageResource(R.drawable.ic_modelado3d_nobg);
-            btn2d.setImageResource(R.drawable.ic_diseno2d_nobg);
-            btnvideo.setImageResource(R.drawable.ic_fotografia);
-            category="video";
-        });
+        }
 
 
-        //boton del menu que lleva al perfil
-        btnProfile.setOnClickListener(v->{
-
-            Intent intent = new Intent(this,ProfileActivity.class);
-            startActivity(intent);
-        });
     }
 }
--- app/src/main/java/com/joctypo/finaleco/ProjectAdapter.java
+++ app/src/main/java/com/joctypo/finaleco/ProjectAdapter.java
@@ -1,4 +1,82 @@
 package com.joctypo.finaleco;
 
-public class ProjectAdapter {
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+public class ProjectAdapter extends BaseAdapter {
+
+    ArrayList<Project> projectArrayList;
+
+    public ProjectAdapter(){
+
+    projectArrayList = new ArrayList<>();
+
+    }
+    @Override
+    public int getCount() {
+        return projectArrayList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return projectArrayList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        convertView = layoutInflater.inflate(R.layout.projects_for_me, null);
+        TextView proyectDescription = convertView.findViewById(R.id.tvProjectDescription);
+        ImageView imgCategory = convertView.findViewById(R.id.imgCategory);
+        TextView tvDate = convertView.findViewById(R.id.tvDate);
+        proyectDescription.setText(projectArrayList.get(position).getProjectDescription());
+        switch(projectArrayList.get(position).getCategory()){
+
+            case "diseño2D":
+                imgCategory.setImageResource(R.drawable.ic_diseno_2d);
+                break;
+            case "modelado3D":
+                imgCategory.setImageResource(R.drawable.ic_diseno_3d);
+
+            case "video":
+
+                imgCategory.setImageResource(R.drawable.ic_fotografia);
+                break;
+        }
+        DateFormat df = new SimpleDateFormat("MMM");
+        String month = df.format(projectArrayList.get(position).getMonth());
+        Log.e("TAG", month );
+
+        tvDate.setText(month+""+ projectArrayList.get(position).getDay());
+
+        return convertView;
+    }
+
+    public void AddNewProject(Project project) {
+
+        projectArrayList.add(project);
+        notifyDataSetChanged();
+
+    }
+
+    public void Clear(){
+
+        projectArrayList.clear();
+    }
+
 }
